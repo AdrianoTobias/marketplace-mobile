03f2c319f46960b03fb0c85d7c2a7824137b034c
Adriano <adrianoact@msn.com>
2025-03-15 18:56:51 -0300

    install Gluestack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,16 @@
 import { useFonts } from '@hooks/useFonts'
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed'
 
 export default function RootLayout() {
   const fontsLoaded = useFonts()
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <StatusBar backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+    <GluestackUIProvider>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <StatusBar backgroundColor="transparent" translucent />
+        {fontsLoaded ? <Text>Home</Text> : <View />}
+      </View>
+    </GluestackUIProvider>
   )
 }
