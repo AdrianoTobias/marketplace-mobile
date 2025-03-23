a9f55d9ecc4ffb5747ad513fd25d2da66e654b3a
Adriano <adrianoact@msn.com>
2025-03-23 20:13:16 -0300

    add ProfileScreen form validation

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -5,13 +5,80 @@ import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { useUserPhoto } from '@hooks/useUserPhoto'
 import { LogOut, User, Phone, Mail, KeyRound } from 'lucide-react-native'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import z from 'zod'
+
+const phoneRegex = /^\d{10,11}$/ // Entre 10 e 11 dígitos
+
+const profileFormSchema = z
+  .object({
+    fullName: z
+      .string({
+        required_error: 'Informe o seu nome completo',
+      })
+      .min(3, 'Informe o seu nome completo'),
+    phone: z
+      .string({
+        required_error: 'Informe o seu telefone',
+      })
+      .regex(phoneRegex, 'Telefone inválido'),
+    email: z
+      .string({
+        required_error: 'Informe o seu e-mail',
+      })
+      .email('E-mail inválido'),
+    password: z
+      .string({
+        required_error: 'Informe a senha atual',
+      })
+      .min(1, 'Informe a senha atual'),
+    newPassword: z
+      .string({
+        required_error: 'Informe a nova senha',
+      })
+      .min(3, 'A nova senha deve ter pelo menos 3 caracteres'),
+    newPasswordConfirmation: z
+      .string({
+        required_error: 'Confirme a nova senha',
+      })
+      .min(1, 'Confirme a nova senha'),
+  })
+  .refine((data) => data.newPassword === data.newPasswordConfirmation, {
+    path: ['newPasswordConfirmation'],
+    message: 'A confirmação da nova senha não coincide',
+  })
+
+type ProfileForm = z.infer<typeof profileFormSchema>
 
 export default function ProfileScreen() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ProfileForm>({ resolver: zodResolver(profileFormSchema) })
+
   const { userPhoto, handleUserPhotoSelect } = useUserPhoto()
 
   function handleLogOut() {}
 
-  function handleUpdateProfile() {}
+  function handleUpdateProfile({
+    fullName,
+    phone,
+    email,
+    password,
+    newPassword,
+    newPasswordConfirmation,
+  }: ProfileForm) {
+    console.log({
+      fullName,
+      phone,
+      email,
+      password,
+      newPassword,
+      newPasswordConfirmation,
+    })
+  }
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
@@ -43,18 +110,37 @@ export default function ProfileScreen() {
 
         <View flex={1} px={'$10'} pb={'$4'} gap={'$5'}>
           <Center gap={'$5'}>
-            <Input
-              label="Nome"
-              id="name"
-              icon={User}
-              placeholder="Seu nome completo"
+            <Controller
+              control={control}
+              name="fullName"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  variant="underlined"
+                  label="Nome"
+                  icon={User}
+                  placeholder="Seu nome completo"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.fullName?.message}
+                />
+              )}
             />
-            <Input
-              label="Telefone"
-              id="phone"
-              icon={Phone}
-              placeholder="(00) 00000-0000"
-              keyboardType="phone-pad"
+
+            <Controller
+              control={control}
+              name="phone"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  variant="underlined"
+                  label="Telefone"
+                  icon={Phone}
+                  placeholder="(00) 00000-0000"
+                  keyboardType="phone-pad"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.phone?.message}
+                />
+              )}
             />
           </Center>
 
@@ -68,41 +154,83 @@ export default function ProfileScreen() {
               Acesso
             </Text>
 
-            <Input
-              label="E-mail"
-              id="mail"
-              icon={Mail}
-              placeholder="mail@exemplo.br"
-              keyboardType="email-address"
-              autoCapitalize="none"
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  variant="underlined"
+                  label="E-mail"
+                  icon={Mail}
+                  placeholder="mail@exemplo.br"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.email?.message}
+                />
+              )}
             />
-            <Input
-              label="Senha atual"
-              id="currentPassword"
-              type="password"
-              icon={KeyRound}
-              placeholder="Sua senha"
-              secureTextEntry
+
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  variant="underlined"
+                  label="Senha atual"
+                  id="currentPassword"
+                  icon={KeyRound}
+                  placeholder="Sua senha"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
             />
-            <Input
-              label="Nova senha"
-              id="newPassword"
-              type="password"
-              icon={KeyRound}
-              placeholder="Sua nova senha"
-              secureTextEntry
+
+            <Controller
+              control={control}
+              name="newPassword"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  variant="underlined"
+                  label="Nova senha"
+                  type="password"
+                  icon={KeyRound}
+                  placeholder="Sua nova senha"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.newPassword?.message}
+                />
+              )}
             />
-            <Input
-              label="Confirmar nova senha"
-              id="newPasswordConfirmation"
-              type="password"
-              icon={KeyRound}
-              placeholder="Confirme sua nova senha"
-              secureTextEntry
+
+            <Controller
+              control={control}
+              name="newPasswordConfirmation"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  variant="underlined"
+                  label="Confirmar nova senha"
+                  id="newPasswordConfirmation"
+                  type="password"
+                  icon={KeyRound}
+                  placeholder="Confirme sua nova senha"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.newPasswordConfirmation?.message}
+                  onSubmitEditing={handleSubmit(handleUpdateProfile)}
+                  returnKeyType="send"
+                />
+              )}
             />
           </View>
 
-          <Button title="Atualizar cadastro" onPress={handleUpdateProfile} />
+          <Button
+            title="Atualizar cadastro"
+            onPress={handleSubmit(handleUpdateProfile)}
+          />
         </View>
       </View>
     </ScrollView>
