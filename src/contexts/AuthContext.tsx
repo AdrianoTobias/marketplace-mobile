558dafce05d7ab1c4da1e29e3781c7604938e076
Adriano <adrianoact@msn.com>
2025-03-27 20:28:18 -0300

    store auth token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,10 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
 import { api } from '@services/api'
+import {
+  storageAuthTokenSave,
+  storageAuthTokenGet,
+  storageAuthTokenRemove,
+} from '@storage/storageAuthToken'
 import { SellerDTO } from '@dtos/SellerDTO'
 import {
   storageSellerGet,
@@ -27,12 +32,28 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [isLoadingSellerStorageData, setIsLoadingSellerStorageData] =
     useState(true)
 
+  async function sellerAndTokenUpdate(sellerData: SellerDTO, token: string) {
+    api.defaults.headers.common.Authorization = `Bearer ${token}`
+
+    setSeller(sellerData)
+  }
+
+  async function storageSellerAndTokenSave(
+    sellerData: SellerDTO,
+    token: string,
+  ) {
+    setIsLoadingSellerStorageData(true)
+
+    await storageSellerSave(sellerData)
+    await storageAuthTokenSave(token)
+
+    setIsLoadingSellerStorageData(false)
+  }
+
   async function signIn(email: string, password: string) {
     const { data } = await api.post('/sellers/sessions', { email, password })
 
     if (data.accessToken) {
-      // setSeller(data.seller)
-      // storageSellerSave(data.seller)
       const seller = {
         id: '1',
         name: 'Adriano',
@@ -41,8 +62,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
         avatar: '',
       }
 
-      setSeller(seller)
-      storageSellerSave(seller)
+      await storageSellerAndTokenSave(seller, data.accessToken)
+      sellerAndTokenUpdate(seller, data.accessToken)
     }
   }
 
@@ -51,15 +72,19 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
     setSeller({} as SellerDTO)
     await storageSellerRemove()
+    await storageAuthTokenRemove()
 
     setIsLoadingSellerStorageData(false)
   }
 
   async function loadSellerData() {
-    const sellerLogged = await storageSellerGet()
+    setIsLoadingSellerStorageData(true)
 
-    if (sellerLogged) {
-      setSeller(sellerLogged)
+    const sellerLogged = await storageSellerGet()
+    const token = await storageAuthTokenGet()
+
+    if (token && sellerLogged) {
+      sellerAndTokenUpdate(sellerLogged, token)
     }
 
     setIsLoadingSellerStorageData(false)
--- src/storage/storageSeller.ts
+++ src/storage/storageSeller.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { SellerDTO } from '@dtos/SellerDTO'
-import { SELLER_STORAGE } from './storageConfig'
+import { SELLER_STORAGE } from '@storage/storageConfig'
 
 export async function storageSellerSave(seller: SellerDTO) {
   await AsyncStorage.setItem(SELLER_STORAGE, JSON.stringify(seller))
