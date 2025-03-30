a59386f3c24491b5fdd26b06211fd94f7b75a437
Adriano <adrianoact@msn.com>
2025-03-30 15:36:01 -0300

    fix loading display in product details

--- src/app/(app)/product/[id].tsx
+++ src/app/(app)/product/[id].tsx
@@ -128,169 +128,179 @@ export default function ProductDetailsScreen() {
 
   return (
     <View flex={1} bg={'$background'}>
-      <View flex={1} gap={'$4'} px={'$6'} mt="$16">
-        <TouchableOpacity onPress={handleGoBack}>
-          <View flexDirection="row" gap={'$2'} p={2} alignItems="flex-end">
-            <Icon as={MoveLeft} color="$orangeBase" size={'lg'} />
+      {isLoadingProductDetails ? (
+        <Loading />
+      ) : (
+        <View flex={1}>
+          <View flex={1} gap={'$4'} px={'$6'} mt="$16">
+            <TouchableOpacity onPress={handleGoBack}>
+              <View flexDirection="row" gap={'$2'} p={2} alignItems="flex-end">
+                <Icon as={MoveLeft} color="$orangeBase" size={'lg'} />
 
-            <Text
-              color="$orangeBase"
-              fontFamily="$action"
-              fontSize={'$action_sm'}
-              lineHeight={16}
-            >
-              Voltar
-            </Text>
-          </View>
-        </TouchableOpacity>
+                <Text
+                  color="$orangeBase"
+                  fontFamily="$action"
+                  fontSize={'$action_sm'}
+                  lineHeight={16}
+                >
+                  Voltar
+                </Text>
+              </View>
+            </TouchableOpacity>
 
-        {isLoadingProductDetails ? (
-          <Loading />
-        ) : (
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <Image
-              w="$full"
-              h={197}
-              mb={'$8'}
-              rounded={6}
-              source={
-                product?.attachments[0]?.url ? product.attachments[0].url : ''
-              }
-            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+              <Image
+                w="$full"
+                h={197}
+                mb={'$8'}
+                rounded={6}
+                source={
+                  product?.attachments[0]?.url ? product.attachments[0].url : ''
+                }
+              />
 
-            <View flex={1} gap={'$7'}>
-              <View gap={'$4'}>
-                <View flexDirection="row" justifyContent="space-between">
-                  <Text
-                    color="$gray400"
-                    fontFamily="$heading"
-                    fontSize={'$title_md'}
-                    flex={1}
-                  >
-                    {product.title}
-                  </Text>
-
-                  <View flexDirection="row" gap={'$1'} alignItems="center">
+              <View flex={1} gap={'$7'}>
+                <View gap={'$4'}>
+                  <View flexDirection="row" justifyContent="space-between">
                     <Text
-                      color="$gray500"
-                      fontFamily="$label"
-                      fontSize={'$label_md'}
-                    >
-                      R$
-                    </Text>
-
-                    <Text
-                      color="$gray500"
+                      color="$gray400"
                       fontFamily="$heading"
                       fontSize={'$title_md'}
-                      lineHeight={'$title_md'}
+                      flex={1}
                     >
-                      {centsToPrice(product.priceInCents)}
+                      {product.title}
                     </Text>
+
+                    <View flexDirection="row" gap={'$1'} alignItems="center">
+                      <Text
+                        color="$gray500"
+                        fontFamily="$label"
+                        fontSize={'$label_md'}
+                      >
+                        R$
+                      </Text>
+
+                      <Text
+                        color="$gray500"
+                        fontFamily="$heading"
+                        fontSize={'$title_md'}
+                        lineHeight={'$title_md'}
+                      >
+                        {centsToPrice(product.priceInCents)}
+                      </Text>
+                    </View>
                   </View>
-                </View>
 
-                <Text color="$gray400" fontFamily="$body" fontSize={'$body_sm'}>
-                  {product.description}
-                </Text>
-              </View>
-
-              <View gap={6}>
-                <Text
-                  color="$gray500"
-                  fontFamily="$heading"
-                  fontSize={'$title_xs'}
-                >
-                  Categoria
-                </Text>
-
-                <Text color="$gray400" fontFamily="$body" fontSize={'$body_xs'}>
-                  {product.category.title}
-                </Text>
-              </View>
-
-              <View
-                flexDirection="row"
-                h={60}
-                gap={'$3'}
-                pl={'$3'}
-                pr={'$4'}
-                py={'$3'}
-                rounded={10}
-                alignItems="center"
-                bg={'$blueLigth'}
-              >
-                <View bg={'$blueDark'} p={'$2'} rounded={6}>
-                  <Icon as={ChartColumn} color="$white" size={'lg'} />
-                </View>
-
-                {isLoadingProductViewsInTheLastSevenDays ? (
-                  <Loading />
-                ) : (
                   <Text
-                    flex={1}
+                    color="$gray400"
+                    fontFamily="$body"
+                    fontSize={'$body_sm'}
+                  >
+                    {product.description}
+                  </Text>
+                </View>
+
+                <View gap={6}>
+                  <Text
+                    color="$gray500"
+                    fontFamily="$heading"
+                    fontSize={'$title_xs'}
+                  >
+                    Categoria
+                  </Text>
+
+                  <Text
                     color="$gray400"
                     fontFamily="$body"
                     fontSize={'$body_xs'}
-                    lineHeight={16.8}
                   >
+                    {product.category.title}
+                  </Text>
+                </View>
+
+                <View
+                  flexDirection="row"
+                  h={60}
+                  gap={'$3'}
+                  pl={'$3'}
+                  pr={'$4'}
+                  py={'$3'}
+                  rounded={10}
+                  alignItems="center"
+                  bg={'$blueLigth'}
+                >
+                  <View bg={'$blueDark'} p={'$2'} rounded={6}>
+                    <Icon as={ChartColumn} color="$white" size={'lg'} />
+                  </View>
+
+                  {isLoadingProductViewsInTheLastSevenDays ? (
+                    <Loading />
+                  ) : (
                     <Text
-                      fontWeight="bold"
+                      flex={1}
                       color="$gray400"
                       fontFamily="$body"
-                      fontSize={'$body_sm'}
+                      fontSize={'$body_xs'}
                       lineHeight={16.8}
                     >
-                      {productViews} pessoas
-                    </Text>{' '}
-                    visualizaram este produto nos últimos 7 dias
-                  </Text>
-                )}
+                      <Text
+                        fontWeight="bold"
+                        color="$gray400"
+                        fontFamily="$body"
+                        fontSize={'$body_sm'}
+                        lineHeight={16.8}
+                      >
+                        {productViews} pessoas
+                      </Text>{' '}
+                      visualizaram este produto nos últimos 7 dias
+                    </Text>
+                  )}
+                </View>
               </View>
-            </View>
-          </ScrollView>
-        )}
-      </View>
+            </ScrollView>
+          </View>
 
-      <View
-        flexDirection="row"
-        h={96}
-        p={'$6'}
-        pb={'$8'}
-        bg={'$white'}
-        alignItems="center"
-        justifyContent="space-between"
-        style={{
-          elevation: 10,
-          shadowColor: 'black',
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.8,
-          shadowRadius: 2,
-        }}
-      >
-        <View flexDirection="row" gap={'$1'} alignItems="flex-end">
-          <Text color="$gray500" fontFamily="$label" fontSize={'$label_md'}>
-            R$
-          </Text>
-
-          <Text
-            color="$gray500"
-            fontFamily="$heading"
-            fontSize={'$title_lg'}
-            lineHeight={'$title_lg'}
+          <View
+            flexDirection="row"
+            h={96}
+            p={'$6'}
+            pb={'$8'}
+            bg={'$white'}
+            alignItems="center"
+            justifyContent="space-between"
+            style={{
+              elevation: 10,
+              shadowColor: 'black',
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.8,
+              shadowRadius: 2,
+            }}
           >
-            {centsToPrice(product.priceInCents)}
-          </Text>
-        </View>
+            <View flexDirection="row" gap={'$1'} alignItems="flex-end">
+              <Text color="$gray500" fontFamily="$label" fontSize={'$label_md'}>
+                R$
+              </Text>
 
-        <Button
-          title="Entrar em contato"
-          width={172}
-          height={40}
-          fontSize={14}
-          onPress={handlePhoneContact}
-        />
-      </View>
+              <Text
+                color="$gray500"
+                fontFamily="$heading"
+                fontSize={'$title_lg'}
+                lineHeight={'$title_lg'}
+              >
+                {centsToPrice(product.priceInCents)}
+              </Text>
+            </View>
+
+            <Button
+              title="Entrar em contato"
+              width={172}
+              height={40}
+              fontSize={14}
+              onPress={handlePhoneContact}
+            />
+          </View>
+        </View>
+      )}
     </View>
   )
 }
